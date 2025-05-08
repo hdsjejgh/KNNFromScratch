17b74b00a6ef820710a418b4ffc13daf160d7f9a
hdsjejgh <aayanahmad08@hotmail.com>
2025-05-08 19:22:05 -0400

    Added KDTree algorithm and fixed bug when K > number of valid points

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ DIST_METHOD = {0:'euclidian',1:'manhattan'}[0]
 COLOR_CONV = {0:'red',1:'blue',2:'green'}
 Xs = []
 Ys = []
-
+medians = []
+tree = [[[],[]],[[],[]]]
+print(tree)
 
 with open(PATH) as file:
     reader = csv.reader(file)
@@ -20,6 +22,14 @@ with open(PATH) as file:
         Xs.append(features)
         Ys.append(label)
         plt.scatter(features[0],features[1],color=COLOR_CONV[label])
+for i in range(len(Xs[0])):
+    s = sorted(Xs,key=lambda x:x[i])
+    medians.append(s[len(s)//2][i])
+plt.axvline(x=medians[0],color='black')
+plt.axhline(y=medians[1],color='black')
+for idx,p in enumerate(Xs):
+    indexes = [1 if p[i]>medians[i] else 0 for i in range(len(p))]
+    tree[indexes[0]][indexes[1]].append((idx,p))
 plt.show()
 
 def distance(p1,p2):
@@ -33,21 +43,24 @@ def distance(p1,p2):
         print("INVALID DISTANCE METHOD")
         exit(-1)
 
-def basicKN(point):
+def BasicKN(point):
     count = [ (0,distance(Xs[0],point)) ]
-    for idx,p in enumerate(Xs):
-        if idx==0:
-            continue
-        dist = distance(point,p)
-        if dist<count[-1][1]:
-            id = 0
-            for i in range(len(count)):
-                id = i
-                if count[i][1]>dist:
-                    break
-            count.insert(id,(idx,dist))
-            if len(count) > NUM_NEIGHBORS:
-                count.pop()
+    if len(Xs) > NUM_NEIGHBORS:
+        for idx,p in enumerate(Xs):
+            if idx==0:
+                continue
+            dist = distance(point,p)
+            if dist<count[-1][1]:
+                id = 0
+                for i in range(len(count)):
+                    id = i
+                    if count[i][1]>dist:
+                        break
+                count.insert(id,(idx,dist))
+                if len(count) > NUM_NEIGHBORS:
+                    count.pop()
+    else:
+        count = [(idx, distance(point, p)) for idx,p in enumerate(Xs)]
     ids = set(i[0] for i in count)
     for i,p in enumerate(Xs):
         if i in ids:
@@ -59,8 +72,43 @@ def basicKN(point):
     labels = [Ys[i] for i in ids]
     c = Counter(labels)
     guess = max(c, key=c.get)
-    print(f"that points {COLOR_CONV[guess]}, i'm like {100*c[guess]/NUM_NEIGHBORS:.2f}% sure")
+    print(f"that points {COLOR_CONV[guess]}, i'm like {100*c[guess]/len(ids):.2f}% sure")
 
+def KDTree(point):
+    indexes = [1 if point[i] > medians[i] else 0 for i in range(len(point))]
+    sector = tree[indexes[0]][indexes[1]]
+    count = [(0, distance(sector[0][1], point))]
+    if len(sector) > NUM_NEIGHBORS:
+        for idx, p in sector:
+            if idx == 0:
+                continue
+            dist = distance(point, p)
+            if dist < count[-1][1]:
+                id = 0
+                for i in range(len(count)):
+                    id = i
+                    if count[i][1] > dist:
+                        break
+                count.insert(id, (idx, dist))
+                if len(count) > NUM_NEIGHBORS:
+                    count.pop()
+    else:
+        count = [(p[0],distance(point,p[1])) for p in sector]
+    ids = set(i[0] for i in count)
+    for i, p in enumerate(Xs):
+        if i in ids:
+            plt.scatter(p[0], p[1], color=COLOR_CONV[Ys[i]])
+            continue
+        plt.scatter(p[0], p[1], color='grey')
+    plt.scatter(point[0], point[1], color='black')
+    plt.axvline(x=medians[0], color='black')
+    plt.axhline(y=medians[1], color='black')
+    plt.show()
+    labels = [Ys[i] for i in ids]
+    c = Counter(labels)
+    guess = max(c, key=c.get)
 
+    print(f"that points {COLOR_CONV[guess]}, i'm like {100 * c[guess] / len(ids):.2f}% sure")
 
-basicKN((50,50))
+BasicKN((54,60))
+KDTree((54,60))
